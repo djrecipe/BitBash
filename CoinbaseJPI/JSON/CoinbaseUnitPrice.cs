afbb20e2b5a38c3446b9dd2ba5b4d0c19f6e00b6
djrecipe <brkfstmnchr@gmail.com>
2018-01-11 21:20:16 -0800

    added missing file

---
+++ CoinbaseJPI/JSON/CoinbaseUnitPrice.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+
+namespace Abaci.JPI.Coinbase.JSON
+{
+    [JsonObject(MemberSerialization.OptIn)]
+    [Endpoint(SubPath = "prices", SingleToken = "data")]
+    public class CoinbaseUnitPrice
+    {
+        #region Instance Properties
+        [JsonProperty("currency")]
+        public string Currency { get; set; }
+        [JsonProperty("amount")]
+        private string _Price
+        {
+            set
+            {
+                double dbl_value = 0.0;
+                double.TryParse(value, out dbl_value);
+                this.Price = dbl_value;
+                return;
+            }
+        }
+        public double Price { get; set; }
+        #endregion
+    }
+}
